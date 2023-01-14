0adeae51c12881e7a15d24cb2bc7a21f2c595e1a
Theautis <116971196+tptheautis@users.noreply.github.com>
2023-01-14 12:40:33 -0800

    implemented different loop function for our js divs that will give us more flexibility in the future

--- grid.js
+++ grid.js
@@ -1,9 +1,15 @@
+//Adding divs
 const container = document.getElementById('container');
-const div = document.createElement('div');
-let i=1;
+const grid = {rows: 16, columns: 16}
+const total = grid.rows * grid.columns;
 
-for (i = 1; i < 256; i ++) {
-    container.append(div.cloneNode(true));
+createGrid(total);
+
+function createGrid(total) {
+    for (let i = 0; i < total; i++) {
+        const element = document.createElement('div');
+        container.append(element);
+        element.textContent = `${i+1}`;
+        element.classList.add('box');
+    }
 }
-
-
