722aa4629ca997b956f207647f7bcf117a1386bf
Theautis <116971196+tptheautis@users.noreply.github.com>
2023-01-16 12:45:20 -0800

    added buttons with event listeners in js

--- grid.js
+++ grid.js
@@ -2,6 +2,27 @@ const container = document.querySelector('.container');
 const grid = {rows: 8, columns: 8}
 const total = grid.rows * grid.columns;
 
+//Buttons
+const draw = document.getElementById('draw');
+const rainbowMode = document.getElementById('rainbow-mode');
+const eraser = document.getElementById('eraser');
+const clear = document.getElementById('clear');
+
+draw.addEventListener('click', btn => {
+    console.log('draw');
+})
+
+rainbowMode.addEventListener('click', btn => {
+    console.log('rainbowMode');
+})
+
+eraser.addEventListener('click', btn => {
+    console.log('eraser');
+})
+
+clear.addEventListener('click', btn => {
+    console.log('clear');
+})
 
 //Grid
 createGrid(total);
@@ -16,10 +37,13 @@ function createGrid(total) {
     container.style.setProperty(`grid-template-columns`, `repeat(${grid.columns},1fr)`);
 }
 
-
+//mouse
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 //Change div color
-const boxes = document.getElementsByClassName('box');
+/*const boxes = document.getElementsByClassName('box');
 for (let i=0;i<boxes.length;i++) {
     boxes[i].style.backgroundColor = 'red';
 }
@@ -30,7 +54,32 @@ function background () {
 }
 
 for(let i=0;i<boxes.length;i++) {
-    boxes[i].onclick = function(e) {
+    boxes[i].onmouseover = function(e) {
         e.target.style.backgroundColor=background();
+    }
+}
+
+for(let i=0;i<boxes.length;i++) {
+    if (mouseDown = true) {
+    boxes[i].onmouseover = function(e) {
+        e.target.style.backgroundColor=background();
+    }} 
+}*/
+
+
+const mode = color
+const color = "00FF00"
+
+function colorChange(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (mode === 'rgb') {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `r`
+    } else if (mode === 'color') {
+        e.target.style.backgroundColor = color;
+    } else if (mode === 'eraser') {
+        e.target.style.backgroundColor = '#fefefe'
     }
 }
